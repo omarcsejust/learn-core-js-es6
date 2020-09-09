534f3177267a25b69e2043f4d9de2ceb868c4f4a
omarcsejust <omarcsejust@gmail.com>
2020-09-09 18:14:17 +0600

    # Destructuring

---
+++ destructuring/object_destructure.js
@@ -0,0 +1,58 @@
+const person = {
+    name: "Omar",
+    age: 27,
+    location: "Rajshahi",
+    dog: {
+        name: "Rocky"
+    }
+};
+
+// In normal way: ES5
+// ------------------
+const p_name = person.name;
+const p_age = person.age;
+
+console.log(p_name);
+console.log(p_age);
+
+// In Destructuring
+const {name, age} = person;  // variable name (name, age) must be the key name of object(person)
+
+console.log(name);
+console.log(age);
+
+// Default Assignment
+// ------------------
+/*
+ If key not exist in the object the we can assign a default value for that key
+*/
+const {country = "Bangladesh"} = person;
+console.log(country);
+
+// Rename the vaiable name & (Rename the vaiable name and Assign default value)
+// ------------------------------------------------------------------------
+const {name: myName, age: myAge, country: myCounrty = "India"} = person;
+console.log(myName);
+console.log(myAge);
+console.log(myCounrty);
+
+// Nested Destructuring
+const {
+    dog: {
+        name: dogName = "My Dog Name default value"
+    }
+} = person;
+
+console.log(dogName);
+
+// If nested object not present the assign a default value for that object
+// -----------------------------------------------------------------------
+const {
+    shirt: {
+        color = "Blue"
+    } = {
+        color: "Blue"
+    }
+} = person;
+
+console.log(color);
---
+++ spread-rest-operators/spread.js
@@ -0,0 +1,94 @@
+/**
+ Spread operator does spread out the items of an array as individual arguments
+
+ Basically there are three places, we can use spread
+ 1. In Function Call
+ 2. In Array Literals
+ 3. In Object Literals
+ */
+
+ // 1. In Function Call
+ // -------------------
+ const minVal = Math.min(10, 23, 16, 34, -4);
+ console.log(minVal);
+
+ const temperatures = [10, 23, 16, 34, -4];
+ const minVal2 = Math.min(temperatures)  // will return NaN because min function expects individual arguments
+ console.log(minVal2);
+
+ // So we can use spread operator to make array elements spread out into individual elements
+ const minVal3 = Math.min(...temperatures);
+ console.log(minVal3);
+
+ // As string is iterable so we can apply spread on String
+ console.log(Math.min(..."936857"));
+
+ // 2. In Array Literal
+ // -------------------
+ 
+ const fruits = ["Mango", "Lichi", "Tamarind", "Banana", "Apple", "Orange"];
+ const animals = ["Cow", "Buffalo", "Dove", "Dear"];
+
+ // We want to make a single array
+ const fruits_cow = [fruits, animals];  // This will create a nested array
+ console.log(fruits_cow);
+
+ // So we can user spread operator to concate two into a single array
+ const fruits_cow2 = [...fruits, ...animals, "Can add more elements"];
+ console.log(fruits_cow2);
+
+ // normal array concat() method, as we did above using spread operator
+ fruits_cow3 = fruits.concat(animals);
+ console.log(fruits_cow3);
+
+ // We can use spread operator to make a copy of an array
+ // -----------------------------------------------------
+ const fruits2 = fruits;
+ /*
+ In this way fruits2 will copy the reference of fruits, so if we update fruits2, then original array
+ will also be updated and vice versa like Python
+  */
+
+  fruits2.push("Coconut");
+  console.log(fruits);
+
+  fruits.push("Berry");
+  console.log(fruits2);
+
+  // To solve this proble we can use spread operator
+  const fruits3 = [...fruits];
+  fruits3.push("New Fruit"); // now original array won't be change
+
+
+  // 3. In Object Literal
+  // -----------------
+
+  // Copy an object to a new variable to avoid original array being mutated
+  const user1 = { 
+    name: 'Jen', 
+    age: 22 
+  }; 
+    
+  const clonedUser = { ...user1 }; 
+  console.log(clonedUser); 
+
+  // merging two objects using the spread operator
+
+  const user1 = { 
+    name: 'Jen', 
+    age: 22, 
+  }; 
+  
+  const user2 = { 
+    name: "Andrew", 
+    location: "Philadelphia" 
+  }; 
+  
+  const mergedUsers = {...user1, ...user2, gender: "Male"};
+  /*
+   This is just like below
+   const mergedUsers = {name: "Jen", age: 22, name: "Andrew", location: "Philadelphia", gender: "Male"}
+
+   Here name key value will be overriden by last name key value
+   */
+  console.log(mergedUsers) 
